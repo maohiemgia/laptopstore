97774acd6d6181a33a30766fae317a2df2d0de59
maohiemgia <maohiemgia@gmail.com>
2023-05-26 22:47:32 +0700

    modify env and urlconfig

--- public/js/apiconfig.js
+++ public/js/apiconfig.js
@@ -1,2 +1,2 @@
 // export const API_URL = "https://typhoonstore.cu.ma/api/";
-export const API_URL = "http://127.0.0.1:8000/api/";
+export const API_URL = "https://phplaravel-1007637-3556010.cloudwaysapps.com/api/";
